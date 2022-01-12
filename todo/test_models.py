c50e57a6aa203fafca9e8b1179b6afdcaae11561
nlenno1 <nick.lennon@hotmail.co.uk>
2022-01-12 18:22:49 +0000

    testing coverage check and complete all testing required

--- todo/test_models.py
+++ todo/test_models.py
@@ -7,3 +7,7 @@ class TextModel(TestCase):
     def test_done_default_to_false(self):
         item = Item.objects.create(name='Test ToDo Item')
         self.assertFalse(item.done)
+
+    def test_item_string_method_returns_name(self):
+        item = Item.objects.create(name='Test ToDo Item')
+        self.assertEqual(str(item), 'Test ToDo Item')
